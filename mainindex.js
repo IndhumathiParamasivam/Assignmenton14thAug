3f8ce411cdc841bb10a8416032185474fbc411fe
IndhumathiParamasivam <87922656+IndhumathiParamasivam@users.noreply.github.com>
2021-08-14 22:02:56 +0530

    uploading files

---
+++ mainindex.js
@@ -0,0 +1,16 @@
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.open('GET','https://restcountries.eu/rest/v2/all');
+xmlhttp.onload = () =>
+    {
+        let responsedata = xmlhttp.response;
+        let responseinJSON = JSON.parse(responsedata);
+        let countrieswithexpectedpopulation = responseinJSON.filter(countryDetails => countryDetails.population>10000000)
+                                                            .map( cntry => {
+                    let eachcountry={};
+                    eachcountry["name"]=cntry["name"];
+                    eachcountry["population"]=cntry["population"];
+                    return eachcountry;
+                });
+        console.log(countrieswithexpectedpopulation);
+    }
+xmlhttp.send();
